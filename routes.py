cdb0d8ec45781e8e129957be3b85faf1eb0bb154
Jesse Bacon <jessembacon@me.com>
2015-10-03 00:09:21 -0500

    updated routes and css

--- routes.py
+++ routes.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, url_for, redirect, flash
+from flask import Flask, render_template, url_for, redirect, flash, request
+from models import db
+from forms import SignupForm
 import socket
 
 app = Flask(__name__)
 
 app.secret_key = '12ff9a90818fe6ebe152a67cf052dfd6f09485ed07fa32aec9349cdcc6157642'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
 
 @app.route("/")
 @app.route("/index")
@@ -17,6 +20,14 @@ def about():
     #flash(host)
     #flash(name)
     return render_template('about.html')
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if request.method == 'POST':
+        return "Success!"
+    elif request.method == "GET":
+        return render_template('signup.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
